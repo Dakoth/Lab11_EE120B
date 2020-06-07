0f903b779e30c5bdb513108af45ea12c4800e000
Alfredo Gonzalez <alfredogonzalez1999@gmail.com>
2020-06-07 03:38:38 -0700

    updated main for part 1

--- source/main.c
+++ source/main.c
@@ -58,115 +58,54 @@ unsigned char GetKeypadKey() {
 */		
 
 //Shared variables 
-unsigned char led0_output = 0x00;
-unsigned char led1_output = 0x00;
-unsigned char pause = 0; 
+unsigned char tmpB = 0x00; 
+unsigned char x = 0;
 //end of shared variables 
 
+enum keypad_States {keypad_keypad} ;
+int keypadSMTick(int state) {    	
+	x = GetKeypadKey(); 
 
-//monitors button to PA0 
-//when button pressed, "pause" is toggled 
-enum pauseButtonSM_States { pauseButton_wait, pauseButton_press, pauseButton_release }; 
-int pauseButtonSMTick(int state) {
-	//Local varaibles 
-	unsigned char press = ~PINA & 0x01;
+	switch(state) {
+		case keypad_keypad:
+			state = keypad_keypad;
 
-	switch (state) { 
-		case pauseButton_wait:
-			state = press == 0x01? pauseButton_press: pauseButton_wait; 
-			break; 
-
-		case pauseButton_press: 
-			state = pauseButton_release;
-			break; 
-
-		case pauseButton_release:
-		       state = press == 0x00? pauseButton_wait: pauseButton_press;
-	       		break; 
-
-		default: 
-			state = pauseButton_wait;
-			break;		
-	}
-
-	switch(state) {	//actions 
-		case pauseButton_wait: break;
-		case pauseButton_press:
-			pause = (pause == 0) ? 1 : 0;
+			switch (x) {
+				case '\0': tmpB = 0x1F; break;
+				case '1': tmpB = 0x01; break; 
+				case '2': tmpB = 0x02; break;
+				case '3': tmpB = 0x03; break; 
+				case '4': tmpB = 0x04; break;
+				case '5': tmpB = 0x05; break; 
+				case '6': tmpB = 0x06; break;
+				case '7': tmpB = 0x07; break; 
+				case '8': tmpB = 0x08; break;
+				case '9': tmpB = 0x09; break; 
+				case 'A': tmpB = 0x0A; break;
+				case 'B': tmpB = 0x0B; break; 
+				case 'C': tmpB = 0x0C; break;
+				case 'D': tmpB = 0x0D; break; 
+				case '*': tmpB = 0x0E; break;
+				case '0': tmpB = 0x00; break; 
+				case '#': tmpB = 0x0F; break;
+				default: tmpB = 0x1B; break; //should never occur, middle led
+		
+				}
 			break;
 
-		case pauseButton_release: 
-			break; 
-	}
-	return state; 
-}
-
-
-//toggleLED0
-//if paused: Don't toggle LED connected to PB0
-//if unpaused: toggle LED connected to PB0
-enum toggleLED0_States { toggleLED0_wait, toggleLED0_blink} ;
-
-int toggleLED0SMTick ( int state) {
-	switch (state) {
-		case toggleLED0_wait: 
-			state = !pause? toggleLED0_blink: toggleLED0_wait; 
-			break; 
-	
-		case toggleLED0_blink:
-			state = pause? toggleLED0_wait: toggleLED0_blink; 
-		        break;
-
 		default: 
-	       		state = toggleLED0_wait;
-			break;		
-	}
-
-	switch (state) { 
-		case toggleLED0_wait: break;
-		case toggleLED0_blink:
-			led0_output = (led0_output == 0x00) ? 0x01 : 0x00;
+			state = keypad_keypad;
 			break;
-	}
+		}
+		//switch (state) PROBABLY NONE NEEDED
+		//
 
-	return state; 
+       		return state; 	
 }
 
-
-//toggleLED1
-//paused: don't toggle PB1
-//unpaused: toggle PB1
-enum toggleLED1_States { toggleLED1_wait, toggleLED1_blink} ;
-
-int toggleLED1SMTick ( int state) {
-	switch (state) {
-		case toggleLED1_wait: 
-			state = !pause? toggleLED1_blink: toggleLED1_wait; 
-			break; 
-	
-		case toggleLED1_blink:
-			state = pause? toggleLED1_wait: toggleLED1_blink; 
-		        break;
-
-		default: 
-	       		state = toggleLED1_wait;
-			break;		
-	}
-
-	switch (state) { 
-		case toggleLED1_wait: break;
-		case toggleLED1_blink:
-			led1_output = (led1_output == 0x00) ? 0x01 : 0x00;
-			break;
-	}
-
-	return state; 
-}
 
 //display 
-
 enum display_States { display_display }; 
-
 int displaySMTick(int state) {
 	unsigned char output;
 
@@ -183,7 +122,7 @@ int displaySMTick(int state) {
 
 	switch (state) { //actions 
 		case display_display: 
-			output = led0_output | led1_output << 1; 
+			output = tmpB; 
 			break;
 	}
 	PORTB = output; 
@@ -193,43 +132,31 @@ int displaySMTick(int state) {
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	//DDRB = 0xFF; PORTB = 0x00; //outputs, set to 0
-	//DDRC = 0xF0; PORTC = 0x0F; //PC7-4 outputs, PC3 - 0 inputs
+	DDRB = 0xFF; PORTB = 0x00; //outputs, set to 0
+	DDRC = 0xF0; PORTC = 0x0F; //PC7-4 outputs, PC3 - 0 inputs
 	//unsigned char x; 
 
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
+	//DDRA = 0x00; PORTA = 0xFF;
+	//DDRB = 0xFF; PORTB = 0x00;
 
 	//declar array of tasks 
-	static task task1, task2, task3, task4; 
-	task *tasks[] = { &task1, &task2, &task3, &task4 };
+	static task task1, task2; 
+	task *tasks[] = { &task1, &task2 };
         const unsigned short numTasks = sizeof(tasks)/sizeof(task*);	
 
 	const char start = -1;
 
-	//task 1 (pauseBUttonToggleSM)
+	//task 1 (keypad)
 	task1.state = start; //inital state task 
 	task1.period = 50;	//task period 
 	task1.elapsedTime = task1.period; //task current elapsed time 
-	task1.TickFct = &pauseButtonSMTick; //Function pointer
+	task1.TickFct = &keypadSMTick; //Function pointer
 
-	//task 2 (toggleLED0SM)
+	//task 2 (displaySM)
 	task2.state = start; //inital state task 
-	task2.period = 500;	//task period 
+	task2.period = 10;	//task period 
 	task2.elapsedTime = task2.period; //task current elapsed time 
-	task2.TickFct = &toggleLED0SMTick; //Function pointer
-
-	//task 3 (toggleLED1SM)
-	task3.state = start; //inital state task 
-	task3.period = 1000;	//task period 
-	task3.elapsedTime = task3.period; //task current elapsed time 
-	task3.TickFct = &toggleLED1SMTick; //Function pointer
-
-	//task 4 (displaySM)
-	task4.state = start; //inital state task 
-	task4.period = 10;	//task period 
-	task4.elapsedTime = task4.period; //task current elapsed time 
-	task4.TickFct = &displaySMTick; //Function pointer
+	task2.TickFct = &displaySMTick; //Function pointer
 
 	//GCD VVVV
 	unsigned short i; 

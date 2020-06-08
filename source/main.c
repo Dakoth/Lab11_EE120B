a9008c0f9a432afd8aaa04dc061ddc3a40fa6d68
Alfredo Gonzalez <alfredogonzalez1999@gmail.com>
2020-06-07 17:03:09 -0700

    finished part 1, messing with LCD h files

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: agonz250
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #11  Exercise #
+ *	Lab Section: 028 
+ *	Assignment: Lab #11  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,56 +12,53 @@
 #include "bit.h"
 #include "scheduler.h"
 #include "timer.h" 
+#include "io.h"
 
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-/*
-unsigned char GetKeypadKey() { 
-	PORTC = 0xEF;
-	asm("nop");
-	if (GetBit(PINC, 0) == 0) { return('1'); } 
-	if (GetBit(PINC, 1) == 0) { return('4'); } 
-	if (GetBit(PINC, 2) == 0) { return('7'); } 
-	if (GetBit(PINC, 3) == 0) { return('*'); } 
-
-	//check keys in col 2
-	PORTC = 0xDF; 
-	asm("nop");
-	if (GetBit(PINC, 0) == 0) { return('2'); } 
-	if (GetBit(PINC, 1) == 0) { return('5'); } 
-	if (GetBit(PINC, 2) == 0) { return('8'); } 
-	if (GetBit(PINC, 3) == 0) { return('0'); } 
-
-	//check keys in col 3
-	PORTC = 0xBF; 
-	asm("nop");
-	if (GetBit(PINC, 0) == 0) { return('3'); } 
-	if (GetBit(PINC, 1) == 0) { return('6'); } 
-	if (GetBit(PINC, 2) == 0) { return('9'); } 
-	if (GetBit(PINC, 3) == 0) { return('#'); } 
-
-
-	//checks keys in col 4
-	PORTC = 0x7F; 
-	asm("nop");
-	if (GetBit(PINC, 0) == 0) { return('A'); } 
-	if (GetBit(PINC, 1) == 0) { return('B'); } 
-	if (GetBit(PINC, 2) == 0) { return('C'); } 
-	if (GetBit(PINC, 3) == 0) { return('D'); } 
-
-
-
-	return ('\0'); //default value 
-}
-*/		
-
 //Shared variables 
-unsigned char tmpB = 0x00; 
-unsigned char x = 0;
+//unsigned char tmpB = 0x00; 
+//unsigned char x = 0;
+
+unsigned char msg[] = "CS120B is Legend... wait for it DARY!"; //msg
+unsigned char tmp[50] = "";
+
+unsigned char i = 0;
 //end of shared variables 
 
+
+enum message { mess_message };
+int messageSMTick(int state) {
+	switch(state) { //transitions 
+		case mess_message:
+			state = mess_message;
+
+			if(msg[i] != '\0') { //If haven't reached null char.
+				LCD_Cursor(14);
+				LCD_writeData(msg[i]);
+			}	
+			else {
+				i = 0;
+			}
+			break;
+
+		default: 
+			state = mess_message;
+			break;
+	}
+
+	switch (state) { //actions
+		case mess_message:
+			i++;
+			break;
+	}
+	return state; 
+}
+
+
+/*
 enum keypad_States {keypad_keypad} ;
 int keypadSMTick(int state) {    	
 	x = GetKeypadKey(); 
@@ -102,7 +99,7 @@ int keypadSMTick(int state) {
 
        		return state; 	
 }
-
+*/
 
 //display 
 enum display_States { display_display }; 
@@ -122,22 +119,22 @@ int displaySMTick(int state) {
 
 	switch (state) { //actions 
 		case display_display: 
-			output = tmpB; 
+			//output = tmpB; 
 			break;
 	}
-	PORTB = output; 
+	//PORTB = output; 
 	return state; 
 }
 
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRB = 0xFF; PORTB = 0x00; //outputs, set to 0
-	DDRC = 0xF0; PORTC = 0x0F; //PC7-4 outputs, PC3 - 0 inputs
-	//unsigned char x; 
+	//DDRB = 0xFF; PORTB = 0x00; //outputs, set to 0
+	//DDRC = 0xF0; PORTC = 0x0F; //PC7-4 outputs, PC3 - 0 inputs
+	
+	DDRA = 0xFF; PORTA = 0x00; //LCD data lines (dif from lab 7)
+	DDRD = 0xFF; PORTD = 0x00; //LCD control lines 
 
-	//DDRA = 0x00; PORTA = 0xFF;
-	//DDRB = 0xFF; PORTB = 0x00;
 
 	//declar array of tasks 
 	static task task1, task2; 
@@ -146,17 +143,22 @@ int main(void) {
 
 	const char start = -1;
 
-	//task 1 (keypad)
+	//start the LCD 
+	LCD_init(); 
+
+	//task 1 (message)
 	task1.state = start; //inital state task 
-	task1.period = 50;	//task period 
+	task1.period = 100;	//task period 
 	task1.elapsedTime = task1.period; //task current elapsed time 
-	task1.TickFct = &keypadSMTick; //Function pointer
+	task1.TickFct = &messageSMTick; //Function pointer
 
 	//task 2 (displaySM)
 	task2.state = start; //inital state task 
 	task2.period = 10;	//task period 
 	task2.elapsedTime = task2.period; //task current elapsed time 
 	task2.TickFct = &displaySMTick; //Function pointer
+
+
 
 	//GCD VVVV
 	unsigned short i; 
@@ -182,32 +184,3 @@ int main(void) {
 	}
 	return 0;
    }
- /*
-    while (1) {
-	    
-    	
-	x = GetKeypadKey(); 
-	switch (x) {
-		case '\0': PORTB = 0x1F; break;
-		case '1': PORTB = 0x01; break; 
-		case '2': PORTB = 0x02; break;
-		case '3': PORTB = 0x03; break; 
-		case '4': PORTB = 0x04; break;
-		case '5': PORTB = 0x05; break; 
-		case '6': PORTB = 0x06; break;
-		case '7': PORTB = 0x07; break; 
-		case '8': PORTB = 0x08; break;
-		case '9': PORTB = 0x09; break; 
-		case 'A': PORTB = 0x0A; break;
-		case 'B': PORTB = 0x0B; break; 
-		case 'C': PORTB = 0x0C; break;
-		case 'D': PORTB = 0x0D; break; 
-		case '*': PORTB = 0x0E; break;
-		case '0': PORTB = 0x00; break; 
-		case '#': PORTB = 0x0F; break;
-		default: PORTB = 0x1B; break; //should never occur, middle led
-		
-	}*/
-   // }
-    //return 1;
-//}
--- turnin/agonz250_lab11_part1.c
+++ turnin/agonz250_lab11_part1.c
@@ -17,46 +17,6 @@
 #include "simAVRHeader.h"
 #endif
 
-/*
-unsigned char GetKeypadKey() { 
-	PORTC = 0xEF;
-	asm("nop");
-	if (GetBit(PINC, 0) == 0) { return('1'); } 
-	if (GetBit(PINC, 1) == 0) { return('4'); } 
-	if (GetBit(PINC, 2) == 0) { return('7'); } 
-	if (GetBit(PINC, 3) == 0) { return('*'); } 
-
-	//check keys in col 2
-	PORTC = 0xDF; 
-	asm("nop");
-	if (GetBit(PINC, 0) == 0) { return('2'); } 
-	if (GetBit(PINC, 1) == 0) { return('5'); } 
-	if (GetBit(PINC, 2) == 0) { return('8'); } 
-	if (GetBit(PINC, 3) == 0) { return('0'); } 
-
-	//check keys in col 3
-	PORTC = 0xBF; 
-	asm("nop");
-	if (GetBit(PINC, 0) == 0) { return('3'); } 
-	if (GetBit(PINC, 1) == 0) { return('6'); } 
-	if (GetBit(PINC, 2) == 0) { return('9'); } 
-	if (GetBit(PINC, 3) == 0) { return('#'); } 
-
-
-	//checks keys in col 4
-	PORTC = 0x7F; 
-	asm("nop");
-	if (GetBit(PINC, 0) == 0) { return('A'); } 
-	if (GetBit(PINC, 1) == 0) { return('B'); } 
-	if (GetBit(PINC, 2) == 0) { return('C'); } 
-	if (GetBit(PINC, 3) == 0) { return('D'); } 
-
-
-
-	return ('\0'); //default value 
-}
-*/		
-
 //Shared variables 
 unsigned char tmpB = 0x00; 
 unsigned char x = 0;
